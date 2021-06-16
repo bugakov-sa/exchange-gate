726f02ff3d23cbf9f53c628da6cce4f9f09d7901
sergey <bugakov.sa.88@gmail.com>
2021-06-16 11:31:45 +0300

    Refactoring

--- src/main/java/trading/engine/TradeEngineManager.java
+++ src/main/java/trading/engine/TradeEngineManager.java
@@ -3,38 +3,92 @@ package trading.engine;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import trading.engine.strategy.LogLastMinuteOhlcStrategy;
+import trading.db.TradeRobotRepository;
+import trading.entity.Param;
+import trading.entity.TradeRobot;
 import trading.exchange.ExchangeManager;
+import trading.thread.Worker;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @Component
-public class TradeEngineManager {
+public class TradeEngineManager extends Worker {
 
+    private final Map<UUID, TradeEngine> activeEngines = new HashMap<>();
+
+    @Autowired
+    private TradeRobotRepository tradeRobotRepository;
     @Autowired
     private ExchangeManager exchangeManager;
     @Value("${robot.loop.sleep.millis:5000}")
     private long loopSleepMillis;
 
-    private List<TradeEngine> tradeEngines = new ArrayList<>();
+    public TradeEngineManager() {
+        super("trade-engine-manager");
+    }
 
     @PostConstruct
-    public void startRobots() {
-        TradeEngine tradeEngine = new TradeEngine(
-                "XBT/USD-1min",
-                new LogLastMinuteOhlcStrategy("XBT/USD"),
-                exchangeManager,
-                loopSleepMillis
-        );
-        tradeEngines.add(tradeEngine);
-        tradeEngines.forEach(TradeEngine::start);
+    public void init() {
+        createRobotExamples();
+        start();
     }
 
     @PreDestroy
-    public  void stopRobots() {
-        tradeEngines.forEach(TradeEngine::stop);
+    public void close() {
+        stop();
+    }
+
+    @Override
+    protected long executeLoop() {
+        Iterable<TradeRobot> robots = tradeRobotRepository.findAll();
+        for(TradeRobot robot : robots) {
+            UUID robotId = robot.getId();
+            boolean engineStarted = activeEngines.containsKey(robotId);
+            if(robot.isActive() && !engineStarted) {
+                TradeStrategy strategy = TradeStrategyFactory.create(robot);
+                TradeEngine engine = new TradeEngine(
+                        robot.getName(),
+                        strategy,
+                        exchangeManager,
+                        loopSleepMillis
+                );
+                engine.start();
+                activeEngines.put(robotId, engine);
+            }
+            if(!robot.isActive() && engineStarted) {
+                activeEngines.get(robotId).stop();
+                activeEngines.remove(robotId);
+            }
+        }
+        return loopSleepMillis;
+    }
+
+    @Override
+    protected void beforeFinish() {
+        activeEngines.values().forEach(Worker::stop);
+        activeEngines.clear();
+    }
+
+    private void createRobotExamples() {
+        boolean anyRobotExists = tradeRobotRepository.findAll().iterator().hasNext();
+        if(!anyRobotExists) {
+            TradeRobot robot = new TradeRobot();
+            robot.setName("XBT/USD-1min");
+            robot.setActive(true);
+            robot.setStrategy(TradeRobot.Strategy.LOG_LAST_MINUTE_OHLC);
+            robot.setParams(new ArrayList<>());
+            Param param = new Param();
+            param.setName("pair");
+            param.setType(Param.Type.STRING);
+            param.setValue("XBT/USD");
+            param.setRobot(robot);
+            robot.getParams().add(param);
+            tradeRobotRepository.save(robot);
+        }
     }
 }
